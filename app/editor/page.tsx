111af8c019c68b5030dd427c4c41fbeff6f85f0d
omair <o.vandenbroucke@outlook.com>
2026-05-14 14:45:47 +0200

    fix stale data on page load by forcing dynamic rendering

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,6 +1,8 @@
 import { getPresets } from "@/lib/actions/presets";
 import EditorClient from "./_client";
 
+export const dynamic = "force-dynamic";
+
 export default async function EditorPage() {
   const initialPresets = await getPresets();
   return <EditorClient initialPresets={initialPresets} />;
